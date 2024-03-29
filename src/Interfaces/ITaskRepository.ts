c3e25c60b34280dac934826e5e5930d9951f37bb
Leaundrae Mckinney <pytechdrae@gmail.com>
2024-03-29 15:21:46 -0400

    Implementing repository tasks

--- src/Interfaces/ITaskRepository.ts
+++ src/Interfaces/ITaskRepository.ts
@@ -1,21 +1,43 @@
 import mongoose from "mongoose";
 import { TaskDto } from "../Dtos/TaskDto";
 import { ITaskBoard } from "./ITaskBoard";
+import { SubtaskDto } from "../Dtos/SubtaskDto";
+import TaskboardDto from "../Dtos/TaskboardDto";
+import { IColumn } from "./IColumn";
 
 export interface ITaskManagementRepository {
   getAllBoards: () => Promise<ITaskBoard[]> | Error;
   getBoard: (boardId: string) => Promise<ITaskBoard | null | Error>;
   addBoard: (boardName: string, columns: string[]) => Promise<ITaskBoard>;
-  updateBoard: (boardId: string) => void;
+  updateBoard: (
+    boardId: mongoose.Types.ObjectId,
+    boardName: string,
+    columnNames: string[]
+  ) => void;
   deleteBoard: (boardId: string) => Promise<void>;
 
   getAllTasks: (boardId: string) => Promise<TaskDto[]> | Error;
   addTask: (boardId: string, task: TaskDto) => Promise<mongoose.Types.ObjectId>;
   deleteTask: (taskId: string) => Promise<void>;
-  updateTask: (taskId: string) => void;
+  updateTask: (taskId: string, updatedTask: TaskDto) => void;
 
   addColumn: (
     boardId: mongoose.Types.ObjectId,
     columnName: string | string[]
   ) => Promise<mongoose.Types.ObjectId[]>;
+  deleteColumn: (columnId: mongoose.Types.ObjectId) => Promise<void>;
+  updateColumn: (
+    columnId: mongoose.Types.ObjectId,
+    column: IColumn
+  ) => Promise<mongoose.Types.ObjectId>;
+
+  addSubtask: (
+    taskId: mongoose.Types.ObjectId,
+    updatedSubtask: SubtaskDto
+  ) => Promise<mongoose.Types.ObjectId>;
+  deleteSubtask: (subtaskId: mongoose.Types.ObjectId) => Promise<void>;
+  updateSubtask: (
+    subtaskId: mongoose.Types.ObjectId,
+    updatedSubtask: SubtaskDto
+  ) => Promise<mongoose.Types.ObjectId>;
 }
--- src/Repositories/TaskRepository.ts
+++ src/Repositories/TaskRepository.ts
@@ -8,10 +8,15 @@ import { TaskDto } from "../Dtos/TaskDto";
 import { SubtaskDto } from "../Dtos/SubtaskDto";
 import { ISubtask } from "../Interfaces/ISubtask";
 import mongoose from "mongoose";
+import { IColumn } from "../Interfaces/IColumn";
 
 class TaskManagementRepository implements ITaskManagementRepository {
   constructor() {}
 
+  /**Add a new TaskBoard
+   * @param {string}  boardName - The new name for TaskBoard
+   * @param {string[]} columnNames - The array of column names to be included
+   */
   addBoard = async (
     boardName: string,
     columnNames: string[]
@@ -32,7 +37,63 @@ class TaskManagementRepository implements ITaskManagementRepository {
       throw error;
     }
   };
+  /**
+   * Update information for the Provided TaskBoard
+   * @param {mongoose.Types.ObjectId} boardId - TaskBoard to be updated
+   * @param boardName - New Board name
+   * @param columnNames - Update List of column names
+   * @returns {Promise<ITaskBoard>}
+   */
+  updateBoard = async (
+    boardId: mongoose.Types.ObjectId,
+    boardName: string,
+    columnNames: string[]
+  ) => {
+    try {
+      const boardToUpdate = await TaskBoard.findById(boardId);
 
+      if (!boardToUpdate) {
+        throw new Error("Unable to locate board with id " + boardId);
+      }
+
+      const currentColumns = (await Column.find({
+        taskBoard: boardToUpdate._id,
+      })) as IColumn[];
+
+      const currentColumnNames: string[] = [];
+      const currentColumnIds: mongoose.Types.ObjectId[] = [];
+
+      currentColumns.forEach((column) => {
+        currentColumnNames.push(column.name);
+        currentColumnIds.push(column._id);
+      });
+
+      const columnsToAdd = columnNames.filter(
+        (column) => !currentColumnNames.includes(column)
+      );
+
+      const updatedColumns = [...currentColumnIds];
+
+      for (const column of columnsToAdd) {
+        const newColumn = await this.addColumn(boardToUpdate._id, column);
+        updatedColumns.push(...newColumn);
+      }
+
+      return (await TaskBoard.findByIdAndUpdate(boardId, {
+        name: boardName,
+        columns: updatedColumns,
+      })) as ITaskBoard;
+    } catch (error) {
+      console.error("Unable to update the TaskBoard with id " + boardId);
+      throw error;
+    }
+  };
+  /**
+   * Adds a new Column to a given board
+   * @param {mongoose.Types.ObjectId} boardId - The Id for the TaskBoard Model
+   * @param {string[]} columnNames - Array of Names for the new column(s)
+   * @returns {Promise<mongoose.Types.ObjectId[]>}
+   */
   addColumn = async (
     boardId: mongoose.Types.ObjectId,
     columnNames: string | string[]
@@ -66,6 +127,59 @@ class TaskManagementRepository implements ITaskManagementRepository {
       throw error;
     }
   };
+
+  /**
+   * Deletes the provide column and associated tasks, and subtasks
+   * @param columnId
+   */
+  deleteColumn = async (columnId: mongoose.Types.ObjectId) => {
+    try {
+      const columnToDelete = await Column.findOne({ _id: columnId });
+      if (!columnToDelete) {
+        throw new Error(`Unable to locate a column with id: ${columnId}`);
+      }
+      const tasksToDelete = await Task.find({ columnId: columnId }).select(
+        "_id"
+      );
+      for (const task of tasksToDelete) {
+        await Subtask.deleteMany({ task: task._id });
+        await Task.findByIdAndDelete({ _id: task._id });
+      }
+      await Column.findByIdAndDelete({ _id: columnId });
+    } catch (error) {
+      console.error("Unable to delete Column with id " + columnId);
+      throw error;
+    }
+  };
+  /**
+   * Updates information for the provided Column
+   * @param columnId
+   * @param {IColumn} columnDto
+   */
+  updateColumn = async (columnId: mongoose.Types.ObjectId, column: IColumn) => {
+    try {
+      const columnToUpdate = await Column.findByIdAndUpdate(
+        columnId,
+        {
+          name: column.name,
+          tasks: column.tasks,
+          taskBoard: column.taskBoard,
+        },
+        { new: true }
+      );
+      if (!columnToUpdate) {
+        throw new Error("Unable to locate Column with id: " + columnId);
+      }
+      return columnToUpdate._id;
+    } catch (error) {
+      console.error("Unable to Update Column with id " + columnId);
+      throw error;
+    }
+  };
+  /**
+   * Deletes the provided board and associated Columns, Tasks, and Subtasks
+   * @param boardId - Board to be deleted
+   */
   deleteBoard = async (boardId: string): Promise<void> => {
     try {
       const boardToDelete = await TaskBoard.findById(boardId);
@@ -94,6 +208,10 @@ class TaskManagementRepository implements ITaskManagementRepository {
     }
   };
 
+  /**
+   * Deleted the provided task and associated subtasks
+   * @param taskId
+   */
   deleteTask = async (taskId: string): Promise<void> => {
     try {
       const task = await Task.findById(taskId);
@@ -117,6 +235,10 @@ class TaskManagementRepository implements ITaskManagementRepository {
     }
   };
 
+  /**
+   * Retrieves all available TaskBoards
+   * @returns {Promise<ITaskBoard[]>}
+   */
   getAllBoards = (): Promise<ITaskBoard[]> | Error => {
     try {
       return TaskBoard.find();
@@ -125,6 +247,12 @@ class TaskManagementRepository implements ITaskManagementRepository {
       throw error;
     }
   };
+
+  /**
+   * Retrieves all Tasks for a given TaskBoard
+   * @param boardId - Board to be queried against
+   * @returns {Promise<TaskDto[]>}
+   */
   getAllTasks = async (boardId: string): Promise<TaskDto[]> => {
     try {
       const board = await TaskBoard.findOne({ _id: boardId });
@@ -183,6 +311,12 @@ class TaskManagementRepository implements ITaskManagementRepository {
       throw error;
     }
   };
+
+  /**
+   * Retrieve a TaskBoard by its Id
+   * @param boardId - Board to be queried
+   * @returns {Promise<ITaskBoard | null | Error>}
+   */
   getBoard = (boardId: string): Promise<ITaskBoard | null | Error> => {
     try {
       return TaskBoard.findOne({ _id: boardId });
@@ -192,7 +326,16 @@ class TaskManagementRepository implements ITaskManagementRepository {
     }
   };
 
-  addTask = async (boardId: string, task: TaskDto) => {
+  /**
+   * Add a new Task
+   * @param boardId - Board to be queried
+   * @param task - The new Task to be added
+   * @returns {Promise<mongoose.Types.ObjectId>}
+   */
+  addTask = async (
+    boardId: string,
+    task: TaskDto
+  ): Promise<mongoose.Types.ObjectId> => {
     try {
       const board = await TaskBoard.findOne({ _id: boardId });
       if (!board) {
@@ -237,7 +380,125 @@ class TaskManagementRepository implements ITaskManagementRepository {
       throw error;
     }
   };
-  updateTask = () => {};
-  updateBoard = () => {};
+
+  /**
+   *Update the given task based on the provided TaskDto
+   * @param {string} taskId - The Task to be updated
+   * @param {TaskDto} updatedTask - The xpected Task
+   * @returns {TaskDto}
+   */
+  updateTask = async (taskId: string, updatedTask: TaskDto) => {
+    try {
+      const taskToUpdate = await Task.findById(taskId);
+
+      if (!taskToUpdate) {
+        throw new Error(`Unable to locate task with id: ${taskId}`);
+      }
+
+      const currentSubtasks = taskToUpdate.subtasks.map(
+        (subtask) => subtask._id
+      );
+      const newSubtasks = updatedTask.subtasks.filter(
+        (subtask) =>
+          !currentSubtasks.includes(new mongoose.Types.ObjectId(subtask.id))
+      );
+
+      const subtasksToAdd: mongoose.Types.ObjectId[] = [];
+
+      for (const subtask of newSubtasks) {
+        const newSubtask = await this.addSubtask(taskToUpdate._id, subtask);
+        subtasksToAdd.push(newSubtask._id);
+      }
+
+      await Task.findByIdAndUpdate(taskId, {
+        name: updatedTask.title,
+        description: updatedTask.description,
+        status: updatedTask.status,
+        column: updatedTask.column,
+        subtasks: [...currentSubtasks, ...subtasksToAdd],
+      });
+    } catch (error) {
+      console.error("Failed to update the provided Task");
+      throw error;
+    }
+  };
+
+  /**
+   * Creates a new subtask
+   * @param taskId - Parent Task Id
+   * @param updatedSubtask - Information to be included in the subtask
+   * @returns {mongoose.Types.ObjectId}
+   */
+  addSubtask = async (
+    taskId: mongoose.Types.ObjectId,
+    updatedSubtask: SubtaskDto
+  ) => {
+    try {
+      const newSubtask = await Subtask.create({
+        title: updatedSubtask.title,
+        isCompleted: updatedSubtask.isCompleted,
+        task: taskId,
+      });
+      await Task.findByIdAndUpdate(taskId, {
+        $push: { subtasks: newSubtask._id },
+      });
+      return newSubtask._id;
+    } catch (error) {
+      console.error("Failed to add the subtask");
+      throw error;
+    }
+  };
+
+  /**
+   * Removes the provide subtask from the Database
+   * @param subtaskId - Subtask to be deleted
+   */
+  deleteSubtask = async (subtaskId: mongoose.Types.ObjectId) => {
+    try {
+      const subtaskToDelete = await Subtask.findOne({ _id: subtaskId });
+      if (!subtaskToDelete) {
+        throw new Error(`Unable to locate Subtask with id: ${subtaskId}`);
+      }
+
+      await Task.findByIdAndUpdate(subtaskToDelete.task, {
+        $pull: { subtasks: subtaskToDelete._id },
+      });
+
+      await Subtask.findByIdAndDelete(subtaskToDelete._id);
+    } catch (error) {
+      console.error("Unable to Delete the subtask");
+      throw error;
+    }
+  };
+
+  /**
+   * Updates the provide subtask information
+   * @param subtaskId - Subtask to be updated
+   * @param subtask - Subtask Information to be proved
+   * @returns
+   */
+  updateSubtask = async (
+    subtaskId: mongoose.Types.ObjectId,
+    subtask: SubtaskDto
+  ) => {
+    try {
+      const newSubtask = await Subtask.findByIdAndUpdate(
+        subtaskId,
+        {
+          title: subtask.title,
+          isCompleted: subtask.isCompleted,
+          task: subtask.task,
+        },
+        { new: true }
+      );
+      if (!newSubtask) {
+        throw new Error("Unable to locate subtask with id " + subtaskId);
+      }
+      return newSubtask._id;
+    } catch (error) {
+      console.error("Unable to Update the subtask");
+      throw error;
+    }
+  };
 }
 export default TaskManagementRepository;
